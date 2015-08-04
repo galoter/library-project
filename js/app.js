1e1922b95f34747dc3d9be62c054e3baf03acf29
IchyDha <oat@exciteholidays.com>
2015-08-04 14:50:33 +0700

    for bas

--- js/app.js
+++ js/app.js
@@ -1,9 +1,72 @@
 angular.module('library-project',[]);
 
-angular.module('library-project').controller('AuthorController',['$scope','$http',function($scope,$http){
-	$http.get("http://localhost:8080/testGrails/auther/")
-	.then(function(res){
-		$scope.authors = res.data;
+angular.module('library-project')
+.controller('LibraryController',[function(){
 
-	});
-}]);
+
+}])
+.controller('AuthorController',['$http',function($http){
+
+	var vm = this;
+
+	
+
+	vm.state = 'list';
+
+	vm.clearSelection = function(){
+		vm.state = 'list';
+
+	};
+
+	vm.selectAuthor = function(index){
+		vm.state = 'view';
+		vm.selectedAuthor = vm.authors[index];
+	};
+	vm.createBtn = function(){
+		vm.state = 'create';
+		vm.model = {};
+	};
+	vm.submit = function(){
+
+		if(vm.state === 'create'){
+			createAuthor()
+		}else{
+			updateAuthor();
+		}
+
+		
+	};
+	vm.reload = function(){
+		$http.get("http://localhost:8080/testGrails/auther/")
+		.then(function(res){
+			vm.authors = res.data;
+		});
+	};
+
+	vm.editBtn = function(){
+		vm.state = 'edit';
+		vm.model = angular.copy(vm.selectedAuthor);
+	};
+
+	vm.reload();
+
+	
+	function createAuthor(){
+		$http.post("http://localhost:8080/testGrails/auther/create",vm.model)
+		.then(function(res){
+			vm.state = 'view';
+			vm.reload();
+			vm.selectedAuthor = res.data;
+		});
+	};
+	function updateAuthor(){
+		$http.post("http://localhost:8080/testGrails/auther/update/" + vm.model.id,vm.model)
+		.then(function(res){
+			vm.state = 'view';
+			vm.reload();
+			vm.selectedAuthor = res.data;
+		});
+	};
+
+}])
+;
